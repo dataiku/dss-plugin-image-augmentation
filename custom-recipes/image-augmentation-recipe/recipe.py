06cc7528048e8751b054d544cd81c3186ce66762
Stanislas Guinel <sguinel@MBP-DKU-sguinel.local>
2020-02-07 10:53:19 +0100

    first version

--- custom-recipes/image-augmentation-recipe/recipe.py
+++ custom-recipes/image-augmentation-recipe/recipe.py
@@ -8,8 +8,6 @@ import keras
 from PIL import Image
 from io import BytesIO
 from keras.preprocessing.image import ImageDataGenerator
-import string
-import random
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='Image Augmentation Plugin %(levelname)s - %(message)s')
@@ -18,57 +16,59 @@ input_names = get_input_names_for_role('input_image_folder')
 output_names = get_output_names_for_role('output_image_folder')
 
 scaling_factor = get_recipe_config()['scaling_factor']
+height = get_recipe_config()['height']
+width = get_recipe_config()['width']
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+if height > 1024:
+    height = 1024
+    logger.info("[+] Cast images height to {} pixels".format(height))
+if width > 1024:
+    width = 1024
+    logger.info("[+] Cast images width to {} pixels".format(width))
+
 input_images_folder = dataiku.Folder(input_names[0])
 input_images_filenames = input_images_folder.list_paths_in_partition()
 
 output_folder = dataiku.Folder(output_names[0])
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 datagen = ImageDataGenerator(
-        horizontal_flip=True,
-        vertical_flip=False,
-        rotation_range=30,
-        fill_mode='nearest',
-        brightness_range=(0.7, 1.3),
-        zoom_range = [0.7,1]
-        )
+    zoom_range=0.2,
+    shear_range=0.2,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True
+)
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-def random_string(length):
-    pool = string.letters + string.digits
-    return ''.join(random.choice(pool) for i in range(length))
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 for sample_image in input_images_filenames:
-
-    # Read image from DSS folder as bytes
     try:
         with input_images_folder.get_download_stream(sample_image) as stream:
             data = stream.readlines()
     except:
         raise ('Could not open file: {}'.format(sample_image))
+        logger.warning("WARNING [-] Could not open file: {}".format(sample_image))
         continue
 
     img_bytes = ''.join(data)
-    img_array = np.array(Image.open(BytesIO(img_bytes)))
-    # Add one more dimension (for Keras input flow)
-    img_array = np.expand_dims(img_array, axis=0)
+    # resize image into specified dimensions
+    img_resize = Image.open(BytesIO(img_bytes)).resize((height, width))
+    img_array = np.array(img_resize)
 
-    flow_gen = datagen.flow(img_array, y=None, batch_size=1)
-
-    # Press run cells to view different images
-    for i, img in enumerate(flow_gen):
-        output_im = Image.fromarray(np.uint8(img[0])).resize((500,500))
-        im_resize = output_im.resize((500, 500))
+    for i in range(scaling_factor):
+        img = datagen.random_transform(img_array)
+        new_img = Image.fromarray(np.uint8(img))
 
         buf = BytesIO()
-        im_resize.save(buf, format='JPEG')
+        new_img.save(buf, format='JPEG')
         byte_im = buf.getvalue()
 
-        with output_folder.get_writer("{}.jpg".format(random_string(10))) as w:
+        with output_folder.get_writer(sample_image.split('.')[0] + " _" + str(i) + ".jpg") as w:
             w.write(byte_im)
 
-        if i > scaling_factor:
-            break
+    old_img = Image.fromarray(np.uint8(img_array))
+    buf = BytesIO()
+    old_img.save(buf, format='JPEG')
+    byte_im = buf.getvalue()
+
+    with output_folder.get_writer(sample_image) as w:
+        w.write(byte_im)
